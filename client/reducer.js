78734fc7a9bf46c7cb6ae7632ee56b3112f3e6f6
emontecalvo <emilymontecalvo@gmail.com>
2017-02-05 18:37:18 -0800

    art update

--- client/reducer.js
+++ client/reducer.js
@@ -14,16 +14,15 @@ const initialState = {
 const foodEventOpts = [['Mertle seems to be fairly tame at the moment', './default.png', 0, 0],
 					['One of the flies escapes and charges into your mouth!', './fly_in_mouth.png', 0, 10],
 					['Mertle spits in your face with enormous force!', './spit_out.png', 0, 10],
-					['Mertle snaps at your fingers, saliva drooling from its mouth.', './bully_trap.png', 0, 10],
-					['Mertle knocks you out and steals your pocket money.', './bully_trap.png', 0, 20]
+					['Mertle snaps at your fingers, saliva drooling from its mouth.', './mertle_snap.png', 0, 10],
+					['Mertle knocks you out and steals your pocket money.', './knock_out.png', 0, 20]
 				];
 
 const waterEventOpts = [['Mertle seems to be fairly tame at the moment', './default.png', 0, 0],
 					['Mertle spits in your face with enormous force!', './spit_out.png', 0, 10],
-					['Mertle snaps at your fingers, saliva drooling from its mouth.', './bully_trap', 0, 10],
-					['Mertle is in a bad mood today, and does not accept any water.', './rage_face.png', 0, 10],
+					['Mertle is in a bad mood today, and does not accept any water.', './no_water.png', 0, 10],
 					['Mertle spits out the water in disgust!', './spit_out.png', 10, 10],
-					['Mertle knocks you out and steals your pocket money.', './bully_trap.png', 0, 10]
+					['Mertle knocks you out and steals your pocket money.', './knock_out.png', 0, 10]
 				];
 
 const slapEventOpts = [["Your parents don't like you're attitude.  You must take care of Mertle for an extra day.", './angry_parents.png']
@@ -75,6 +74,7 @@ const reducer = (state, action) => {
 	
 	if (action.type === 'WATER_THING') {
 		console.log("WATER THING REDUCER");
+		state.image = './water_mertle.png';
 		state.days += 1;
 		let maybeEvent = randomWaterEvent();
 		console.log("maybeEvent is", maybeEvent);
@@ -83,6 +83,8 @@ const reducer = (state, action) => {
 			state.image = maybeEvent[1];
 			state.morale -= maybeEvent[3];
 			state.hp -= maybeEvent[2];
+		} else {
+			state.status = "You are able to water Mertle.  She quietly plots your demise."
 		}
 		if (state.days === state.maxDays) {
 			if (state.hp > 50) {
@@ -99,7 +101,7 @@ const reducer = (state, action) => {
 	}
 
 	if (action.type === 'FEED_THING') {
-		state.image = './default.png';
+		state.image = './feed_mertle.png';
 		console.log("FEED THING REDUCER");
 		state.days += 1;
 		let maybeEvent = randomFoodEvent();
@@ -109,6 +111,8 @@ const reducer = (state, action) => {
 			state.image = maybeEvent[1];
 			state.morale -= maybeEvent[3];
 			state.hp -= maybeEvent[2];
+		} else {
+			state.status = "You feed Mertle without event while she hates you."
 		}
 		if (state.days === state.maxDays) {
 			if (state.hp > 50) {
@@ -125,7 +129,7 @@ const reducer = (state, action) => {
 	}
 
 	if (action.type === 'SLAP_THING') {
-		state.image = './default.png';
+		state.image = './smack_mertle.png';
 		console.log("SLAP THING REDUCER");
 		state.days += 1;
 		state.morale += 10;
@@ -135,6 +139,8 @@ const reducer = (state, action) => {
 			state.status = slapEventOpts[0][0];
 			state.maxDays += 1;
 			state.image = slapEventOpts[0][1];
+		} else {
+			state.status = "You smack Mertle and claim revenge."
 		}
 		if (state.days === state.maxDays) {
 			if (state.hp > 50) {
@@ -154,6 +160,8 @@ const reducer = (state, action) => {
 		console.log("skip called");
 		state.days += 1;
 		state.hp -= 1;
+		state.image = "./default.png";
+		state.status = "You are too afraid to go near Mertle. Skip today."
 		if (state.days === state.maxDays) {
 			if (state.hp > 50) {
 				state.status = "You win!";
