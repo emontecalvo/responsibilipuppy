6f476bc253025fd8800e4fd7f5833064b7215c6b
emontecalvo <emilymontecalvo@gmail.com>
2017-02-09 18:05:01 -0800

    updated package json

--- server.js
+++ server.js
@@ -3,25 +3,21 @@ const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.static("build"));
 
+// this is serving up the build folder
+app.use(express.static("build"));
 
 const bodyParser = require('body-parser')
 const mongoose   = require('mongoose')
 
-
-mongoose.connect('mongodb://localhost:27017/puppy');
+// mongoose.connect('mongodb://localhost:27017/puppy');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-
-
 app.get("/hello", (req, res) => {
 	res.json({message: "Hello, Mertle!"});
 });
-
-
 
 function run_server() {
 	app.listen(PORT, HOST, err => {
